0a03a5d39321cf0ffb7654b0797a87bf69424354
Jesse Hattabaugh <jesse.hattabaugh@planet.com>
2015-11-03 10:25:23 -0800

    trying a different API but RxJS doesn't let you update observables

--- stator.js
+++ stator.js
@@ -4,36 +4,54 @@ const rx = require('rx');
 
 module.exports = Stator;
 
-function Stator(element, events, translator, accumulator) {
+function Stator(element) {
+
+  this.element = element;
+  this.accumulators = [];
+  this.translators = [];
+
   // reset localStorage
   //localStorage.setItem('actions', JSON.stringify([]));
 
   // load old actions from localstorage
-  let actions = JSON.parse(localStorage.getItem('actions'));
+  //let actions = JSON.parse(localStorage.getItem('actions'));
+  let actions = [];
 
   // if there were none, create an empty array to store them in
-  if (Object.prototype.toString.call(actions) !== '[object Array]')
-    actions = [];
+  //if (Object.prototype.toString.call(actions) !== '[object Array]')
+  //  actions = [];
 
-  this.initialState = actions.reduce(accumulator, {type:'', todos: []});
+  this.initialState = actions.reduce(function (state, action) {
+    return this.accumulators[action.type](state, action);
+  }, {type:'', todos: []});
 
-  this.stream = rx.Observable.merge(events.map(eventType => rx.Observable.fromEvent(element, eventType)))
+  this.stream = rx.Observable.empty()
     .scan(function (prev, ev) {
-
+      console.info('event occurred');
       // translate the event into an action
-      let action = translator(ev);
+      let action = {type: ev.type}.append(this.translators[ev.type](ev));
 
       // todo: move undo code to method so users can chose their own events
-      if (action.type == 'undo') {
-        let undid = actions.pop();
-        console.log(`undoing ${undid.type}`);
-        localStorage.setItem('actions', JSON.stringify(actions));
-        return actions.reduce(accumulator, {type:'', todos: []});
-      }
-      else {
-        actions.push(action);
-        localStorage.setItem('actions', JSON.stringify(actions));
-        return accumulator(prev, action);
-      }
+      //if (action.type == 'undo') {
+        //let undid = actions.pop();
+        //console.log(`undoing ${undid.type}`);
+        //localStorage.setItem('actions', JSON.stringify(actions));
+        //return actions.reduce(accumulator, {type:'', todos: []});
+      //}
+      //else {
+        //actions.push(action);
+        //localStorage.setItem('actions', JSON.stringify(actions));
+        return this.accumulators[action.type](prev, action);
+      //}
+
     }, this.initialState);
+}
+
+Stator.prototype.observe = function (type, translate, accumulate) {
+  console.dir(arguments);
+  this.stream.merge(rx.Observable.fromEvent(this.element, type));
+
+  // todo: push these onto a stack so that events can have multiple
+  this.translators[type] = translate;
+  this.accumulators[type] = accumulate;
 }
