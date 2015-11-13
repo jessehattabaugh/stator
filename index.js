362dae5ac34fe1761fd4a5ce6a4af13d33d72489
Jesse Hattabaugh <jesse.hattabaugh@planet.com>
2015-11-13 10:20:44 -0800

    ?

--- index.js
+++ index.js
@@ -1,46 +1,42 @@
 'use strict';
 
+// experiment
+
+const rx = require('rx');
+
+rx.Observable.fromEvent(document, 'click')
+  .subscribe(() => alert("this works"));
+
+var ob = rx.Observable.empty();
+ob.subscribe(() => alert("this doesn't work"));
+ob.merge(rx.Observable.fromEvent(document, 'click'));
+
+// experiment
+
 const view = require('./view');
 
 const Stator = require('./stator');
 
 let element = document.body;
 
-let events = ['keyup', 'submit', 'delete', 'undo'];
+let state = new Stator(element);
 
-let state = new Stator(element, events, translator, accumulator);
+state.observe('keyup', (ev) => {input: ev.target.value}, function (state, action) {
+  state.input = action.input;
+  return state
+});
+
+state.observe('submit', (ev) => ev.preventDefault(), function (state, action) {
+  state.todos.push([state.input]);
+  state.input = '';
+  return state;
+});
+
+state.observe('delete', (ev) => {index: ev.index}, function (state, action) {
+  state.todos.splice(action.index, 1);
+  return state;
+});
 
 view.init(element, state.initialState);
 
 state.stream.subscribe(state => view.update(state));
-
-function translator(ev) {
-  var action =  {type: ev.type};
-  switch (action.type) {
-    case 'keyup':
-      action.input = ev.target.value;
-      break;
-    case 'delete':
-      action.index = ev.index;
-      break;
-    default:
-      ev.preventDefault();
-  }
-  return action;
-}
-
-function accumulator(state, action) {
-  switch (action.type) {
-    case 'submit':
-      state.todos.push([state.input]);
-      state.input = '';
-      break;
-    case 'keyup':
-      state.input = action.input;
-      break;
-    case 'delete':
-      state.todos.splice(action.index, 1);
-      break;
-  }
-  return state;
-}
